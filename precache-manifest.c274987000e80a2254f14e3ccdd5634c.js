fc9f64238ee5d1c64c661aa19758ba44eb0a40a8
unknown <marijana308@gmail.com>
2020-03-06 23:00:54 +0100

    Updates

---
+++ precache-manifest.c274987000e80a2254f14e3ccdd5634c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e94635b5bea72a169782714eaa19cf",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "9562914563e298aa0525",
+    "url": "/movies/static/css/main.a415d5e1.chunk.css"
+  },
+  {
+    "revision": "0d4c3d07f278de503b1f",
+    "url": "/movies/static/js/2.818aa32d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movies/static/js/2.818aa32d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9562914563e298aa0525",
+    "url": "/movies/static/js/main.a1a0f5a3.chunk.js"
+  },
+  {
+    "revision": "33baffc18c77ee2ad780",
+    "url": "/movies/static/js/runtime-main.9ef91163.js"
+  }
+]);
